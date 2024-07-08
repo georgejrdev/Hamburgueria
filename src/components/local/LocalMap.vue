78b8c334ca9a6a95e8028dc31cf383b2b0ffad5e
NoxusJr <ctt.georgejr@gmail.com>
2024-07-08 14:00:47 -0300

    Created component: LocalMap

---
+++ src/components/local/LocalMap.vue
@@ -0,0 +1,21 @@
+<template>
+    <div></div>
+</template>
+
+
+<script>
+    export default {
+        name: 'LocalMap'
+    }
+</script>
+
+
+<style scoped>
+    div {
+        margin: auto;
+        width: 80%;
+        height: 50%;
+        background-color: lightgray;
+        border-radius: 40px;
+    }
+</style>
