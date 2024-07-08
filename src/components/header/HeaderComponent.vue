0e7fba877669dc60b5bc048cbedf76717ffa2af5
NoxusJr <ctt.georgejr@gmail.com>
2024-07-08 12:04:21 -0300

    Created Header Component

---
+++ src/components/header/HeaderComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <HeaderTitle />
+    </header>
+</template>
+
+
+<script>
+    import HeaderTitle from './HeaderTitle.vue'
+
+    export default {
+        name: 'HeaderComponent',
+        components: {
+            HeaderTitle,
+        }
+    }
+</script>
+
+
+<style scoped>
+    header {
+        width: 100vw;
+        max-width: 100%;
+        height: 100vh;
+        background: linear-gradient(to bottom, #FF8A00 0%, #FFF 77%);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
