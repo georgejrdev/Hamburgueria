c426eb01efa3f810522994cb97022c1e91686154
NoxusJr <ctt.georgejr@gmail.com>
2024-07-08 14:00:28 -0300

    Created component: LocalTitle

---
+++ src/components/local/LocalTitle.vue
@@ -0,0 +1,26 @@
+<template>
+    <h2>EM <span>TODA</span> ESQUINA</h2>
+</template>
+
+
+<script>
+    export default {
+        name: 'LocalTitle'
+    }
+</script>
+
+
+<style scoped>
+
+    h2 {
+        margin: auto;
+        width: 90vw;
+        font-size: 28px;
+        text-align: center;
+        color: #404040;
+    }
+
+    span {
+        color: rgb(0, 0, 0);
+    }
+</style>
