5c3dcccbee7e5eeab9fc5b680a0453f9835cc1bf
NoxusJr <ctt.georgejr@gmail.com>
2024-07-08 14:01:10 -0300

    Created component: Local

---
+++ src/components/local/LocalComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <section>
+        <LocalTitle />
+        <LocalMap />
+    </section>
+</template>
+
+
+<script>
+    import LocalTitle from './LocalTitle.vue'
+    import LocalMap from './LocalMap.vue'
+
+    export default {
+        name: 'LocalComponent',
+        components: {
+            LocalTitle,
+            LocalMap
+        }
+    }
+</script>
+
+
+<style scoped>
+    section {
+        background: linear-gradient(to bottom, #FFF 0%, #FF8A00 88%);
+        width: 100vw;
+        max-width: 100%;
+        padding-top: 10vh;
+        padding-bottom: 10vh;
+        height: 80vh;
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
