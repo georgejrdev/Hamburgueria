23ee2650205fd2680cc577d6de6ae5066d7e0d05
NoxusJr <ctt.georgejr@gmail.com>
2024-07-08 14:02:18 -0300

    Created component: CardFood

---
+++ src/components/main/CardFood.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="card">
+
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'CardFood'
+    }
+</script>
+
+
+<style scoped>
+    #card {
+        width: 250px;
+        height: 250px;
+        background-color: #EBEBEB;
+        border-radius: 40px;
+    }
+</style>
