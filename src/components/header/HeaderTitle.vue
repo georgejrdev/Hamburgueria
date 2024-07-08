4e2db59da5ad02d3955200db09a58f6c6b42ba64
NoxusJr <ctt.georgejr@gmail.com>
2024-07-08 12:03:32 -0300

    Created Header Title Component

---
+++ src/components/header/HeaderTitle.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1><span>HAMBUR</span>GUERIA</h1>
+</template>
+
+
+<script>
+    export  default {
+        name: 'HeaderTitle'
+    }
+</script>
+
+
+<style scoped>
+    h1 {
+        font-family: "Londrina Solid", sans-serif;
+        text-align: center;
+        font-size: 56px;
+        color: rgb(141, 18, 18);
+    }
+    
+    span{
+        font-family: "Londrina Solid", sans-serif;
+        color: rgb(0, 0, 0);
+    }
+</style>
