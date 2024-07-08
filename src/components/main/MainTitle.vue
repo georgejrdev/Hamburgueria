6fced2e8f2022dcdbf98bd4af8c8b71343776f76
NoxusJr <ctt.georgejr@gmail.com>
2024-07-08 14:02:37 -0300

    Created component: MainTitle

---
+++ src/components/main/MainTitle.vue
@@ -0,0 +1,25 @@
+<template>
+    <h2>A SUA <span>MELHOR</span> ESCOLHA</h2>
+</template>
+
+
+<script>
+    export default {
+        name: 'MainTitle'
+    }
+</script>
+
+
+<style scoped>
+    h2 {
+        margin: auto;
+        width: 90vw;
+        font-size: 28px;
+        text-align: center;
+        color: #353535;
+    }
+
+    span{
+        color: #A72424;
+    }
+</style>
