ee38867b70bcba53633eb358e2660c5b670fca06
NoxusJr <ctt.georgejr@gmail.com>
2024-07-09 10:15:28 -0300

    Created component: HeaderNavBar

---
+++ src/components/header/HeaderNavBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav></nav>
+</template>
+
+
+<script>
+    export default {
+        name: 'HeaderNavBar'
+    }
+</script>
+
+
+<style scoped>
+    nav {
+        width: 100vw;
+        max-width: 100%;
+        height: 4vh;
+        background-color: #000;
+        position: absolute;
+        top:0;
+        left:0;
+    }
+</style>
